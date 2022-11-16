189013db8f46212abfacdf7a065bc2e3464b3f89
Felipe Repker <102967559+feliperepker@users.noreply.github.com>
2022-11-16 20:12:44 -0300

    nlw copa

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,106 @@
+// interface HomeProps {
+//   count: number;
+// }
+import logoImg from '../assets/logo.svg'
+import Image from 'next/image'
+import appPreviewImg from '../assets/app-copa-preview.png'
+import usersAvatar from '../assets/avatares.png'
+import iconCheckImg from '../assets/icon-check.png'
+import { api } from '../lib/axios'
+import { FormEvent, useState } from 'react'
+
+interface HomeProps{
+  poolCount: number;
+  guessCount: number;
+  userCount: number;
+}
+
+
+export default function Home(props: HomeProps) {
+  const [poolTitle, setPoolTitle] = useState('')
+
+  async function createPool(event: FormEvent){
+    event.preventDefault()
+    try{
+      const response = await api.post('/pools', {
+        title: poolTitle,
+      });
+  
+      const { code } = response.data
+      await navigator.clipboard.writeText(code)
+      alert('Bolão criado com sucesso')
+
+      setPoolTitle('')
+    }catch(err){
+      alert('Erro')
+    }
+
+  }
+
+  return (
+   <div className='max-w-[1124px] mt-11 h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
+    <main>
+    <Image src={logoImg} alt="Nlw copa"/>
+    <h1 className='mt-14 text-white text-5xl font-bold leading-tight'>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+    <div className='mt-10 flex items-center gap-2'>
+      <Image src={usersAvatar} alt='' />
+      <strong className='text-gray-100 text-xl'>
+        <span className='text-ignite-500'>+{props.userCount}</span> pessoas já estão usando
+      </strong>
+    </div>
+    <form onSubmit={createPool} className='mt-10 flex gap-2'>
+      <input 
+      className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100'
+      type="text" 
+      required 
+      placeholder='Qual o nome do seu bolão?' 
+      onChange={event => setPoolTitle(event.target.value)}
+      value={poolTitle}
+      />
+      <button className='bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700' type='submit'>Criar meu bolão</button>
+    </form>
+
+    <p className='mt-4 text-sm text-gray-300 leading-relaxed'
+    
+    >Após criar seu bolão, você receberá um código único que poderá usar para convidar seus amigos.</p>
+
+    <div className='mt-10 mb-10 pt-10 border-t border-gray-600 flex items-center justify-between text-gray-100'>
+      <div className='flex items-center gap-6'>
+        <Image src={iconCheckImg} alt=''/>
+        <div className='flex flex-col'>
+          <span className='font-bold text-2xl'>+{props.poolCount}</span>
+          <span>Bolões criados</span>
+        </div>
+      </div>
+
+      <div className='w-px h-14 bg-gray-600'/>
+
+      <div className='flex items-center gap-6'>
+      <Image src={iconCheckImg} alt=''/>
+        <div className='flex flex-col'>
+          <span className='font-bold text-2xl'>+{props.guessCount}</span>
+          <span>Palpites enviados</span>
+        </div>
+      </div>
+    </div>
+    </main>
+    <Image quality={100} src={appPreviewImg} alt="Preview do app da copa" />
+   </div>
+  )
+}
+
+export async function getStaticProps(){
+   const [poolCountResponse, guessCountResponse, userCountResponse] = await Promise.all([
+    api.get('/pools/count'),
+    api.get('/guesses/count'),
+    api.get('/users/count'),
+  ])
+
+  return{
+    props: {
+      poolCount: poolCountResponse.data.count,
+      guessCount: guessCountResponse.data.count,
+      userCount: userCountResponse.data.count,
+    }
+  }
+}
